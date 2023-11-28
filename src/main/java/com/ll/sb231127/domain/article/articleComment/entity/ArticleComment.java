9a68c69a407800dedea0d05ccf7ae43f1a67029a
itonse <govl3dnjs@gmail.com>
2023-11-29 01:10:57 +0900

    feat : step14,16 ArticleComment 엔티티 추가
    - @Transactional 을 추가하여 더티체킹에 의해서 추가된 댓글들 2개가 저장되도록

---
+++ src/main/java/com/ll/sb231127/domain/article/articleComment/entity/ArticleComment.java
@@ -0,0 +1,29 @@
+package com.ll.sb231127.domain.article.articleComment.entity;
+
+import com.ll.sb231127.domain.article.article.entity.Article;
+import com.ll.sb231127.domain.member.member.entity.Member;
+import com.ll.sb231127.global.jpa.baseEntity.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.ManyToOne;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+import static lombok.AccessLevel.PROTECTED;
+
+@Entity
+@SuperBuilder   // 슈퍼빌더 사용하기 위해서는 아래 두 어노테이션이 필수
+@AllArgsConstructor(access = PROTECTED)
+@NoArgsConstructor(access = PROTECTED)
+@Setter
+@Getter
+@ToString(callSuper = true)   // id 까지 문자열로 나오게 하려면 callSuper true로 설정
+public class ArticleComment extends BaseEntity {
+    private String title;
+    private String body;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Member author;    // member_id
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Article article;   // article_id
+}
--- src/main/java/com/ll/sb231127/global/initData/NotProd.java
+++ src/main/java/com/ll/sb231127/global/initData/NotProd.java
@@ -1,12 +1,15 @@
 package com.ll.sb231127.global.initData;
 
+import com.ll.sb231127.domain.article.article.entity.Article;
 import com.ll.sb231127.domain.article.article.service.ArticleService;
 import com.ll.sb231127.domain.member.member.entity.Member;
 import com.ll.sb231127.domain.member.member.service.MemberService;
+import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.transaction.annotation.Transactional;
 
 @Profile("!prod")
 @Configuration
@@ -16,15 +19,22 @@ public class NotProd {
             MemberService memberService,
             ArticleService articleService
     ) {
-        return args -> {
-            Member member1 = memberService.join("user1", "1234").getData();
-            Member member2 = memberService.join("user2", "1234").getData();
+        return new ApplicationRunner() {
+            @Transactional
+            @Override
+            public void run(ApplicationArguments args) {
+                Member member1 = memberService.join("user1", "1234").getData();
+                Member member2 = memberService.join("user2", "1234").getData();
 
-            articleService.write(member1.getId(), "제목1", "내용1");
-            articleService.write(member1.getId(), "제목2", "내용2");
+                Article article1 = articleService.write(member1.getId(), "제목1", "내용1").getData();
+                Article article2 = articleService.write(member1.getId(), "제목2", "내용2").getData();
 
-            articleService.write(member2.getId(), "제목3", "내용3");
-            articleService.write(member2.getId(), "제목4", "내용4");
+                Article article3 = articleService.write(member2.getId(), "제목3", "내용3").getData();
+                Article article4 = articleService.write(member2.getId(), "제목4", "내용4").getData();
+
+                article1.addComment(member1, "댓글1");
+                article1.addComment(member1, "댓글1");
+            }
         };
     }
 }
