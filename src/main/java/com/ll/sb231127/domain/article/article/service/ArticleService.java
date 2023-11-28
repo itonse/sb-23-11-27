27ccf23e45908723b20de770dd16fbe9ad6a73c3
itonse <govl3dnjs@gmail.com>
2023-11-28 21:06:09 +0900

    feat : step3 1번 글의 작성자의 username 이 user1 이라는 것을 검증
    - JPA 방식(객체)방식이 아니기 때문에 코드가 길다.

--- src/main/java/com/ll/sb231127/domain/article/article/service/ArticleService.java
+++ src/main/java/com/ll/sb231127/domain/article/article/service/ArticleService.java
@@ -7,6 +7,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
@@ -25,5 +27,9 @@ public class ArticleService {
 
         return RsData.of("200", "%d번 게시글이 작성되었습니다."
                 .formatted(article.getId()), article);
+    }
+
+    public Optional<Article> findById(long id) {
+        return articleRepository.findById(id);
     }
 }
--- src/test/java/com/ll/sb231127/domain/article/article/sevice/ArticleServiceTest.java
+++ src/test/java/com/ll/sb231127/domain/article/article/sevice/ArticleServiceTest.java
@@ -2,6 +2,8 @@ package com.ll.sb231127.domain.article.article.sevice;
 
 import com.ll.sb231127.domain.article.article.entity.Article;
 import com.ll.sb231127.domain.article.article.service.ArticleService;
+import com.ll.sb231127.domain.member.member.entity.Member;
+import com.ll.sb231127.domain.member.member.service.MemberService;
 import com.ll.sb231127.global.rsData.RsData;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -18,6 +20,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class ArticleServiceTest {
     @Autowired
     private ArticleService articleService;
+    @Autowired
+    private MemberService memberService;
 
     @DisplayName("글 쓰기")
     @Test
@@ -26,5 +30,15 @@ public class ArticleServiceTest {
         Article article = writeRs.getData();
 
         assertThat(article.getId()).isGreaterThan(0L);
+    }
+
+    @DisplayName("1번 글의 작성자의 username이 user1 이다.")
+    @Test
+    void t2() {
+        Article article = articleService.findById(1L).get();
+        long authorId = article.getAuthorId();
+
+        Member member = memberService.findById(authorId).get();
+        assertThat(member.getUsername()).isEqualTo("user1");
     }
 }
