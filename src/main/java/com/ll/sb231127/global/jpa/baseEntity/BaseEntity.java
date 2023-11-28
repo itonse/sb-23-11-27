85d66799419af1eee8c63bfd7a6c17a70cca4ce5
itonse <govl3dnjs@gmail.com>
2023-11-28 22:13:51 +0900

    feat : step6 BaseEntity 도입 (@CreateDate 기능 추가)

--- src/main/java/com/ll/sb231127/global/jpa/baseEntity/BaseEntity.java
+++ src/main/java/com/ll/sb231127/global/jpa/baseEntity/BaseEntity.java
@@ -1,18 +1,21 @@
 package com.ll.sb231127.global.jpa.baseEntity;
 
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import lombok.*;
 import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
 
 import static jakarta.persistence.GenerationType.IDENTITY;
 import static lombok.AccessLevel.PROTECTED;
 
 @MappedSuperclass   // 상속받는 엔티티의 필드를 컬럼으로 인식하게 함
+@EntityListeners(AuditingEntityListener.class)
 @SuperBuilder  // 빌더패턴에서 부모 엔티티와 자식 엔티티를 하나의 빌더로 묶으려면 사용
 @NoArgsConstructor(access = PROTECTED)   // 위의 슈퍼빌더를 사용하기 위해 필요
 @Getter
@@ -23,4 +26,7 @@ public class BaseEntity {
     @GeneratedValue(strategy = IDENTITY)
     @EqualsAndHashCode.Include
     private Long id;
+    @CreatedDate
+    private LocalDateTime createDate;
+    private LocalDateTime modifyDate;
 }
